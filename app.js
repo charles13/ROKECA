2a9743653cad0161fc025f61439ad7b091119efc
Carlos Mata <cmatal17@gmail.com>
2016-11-24 01:06:52 +0000

    Changes in the html, css and js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/*eslint-env node*/
+/*eslint-env node, jquery, browser*/
 
 //------------------------------------------------------------------------------
 // node.js starter application for Bluemix
@@ -7,22 +7,37 @@
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
-
-// cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
 
 // create a new express server
 var app = express();
-
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
-
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
-
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
+
+// Part Above is the webserver section DO NOT TOUCH - 
+
+//CODE
+
+var main = function(){
+    $('.icon-menu').click(function(){
+        $('.menu').animate({
+            left:'0px'
+        },200);
+                    
+        });
+    $('.icon-close').click(function(){
+        $('.menu').animate({
+            left:"-285px"
+        },200);
+   
+    });
+    
+    
+};
