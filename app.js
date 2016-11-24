fe02da5e55c53564af6d9aeaa5f7d5cad4bdf846
Carlos Mata <cmatal17@gmail.com>
2016-11-24 01:37:04 +0000

    Testing videos and pictures, new HTML. JS not working, fix the document.ready

--- app.js
+++ app.js
@@ -6,38 +6,32 @@
 
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
-var express = require('express');
-var cfenv = require('cfenv');
+var express = require("express");
+var cfenv = require("cfenv");
 
 // create a new express server
 var app = express();
 // serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 // start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
+app.listen(appEnv.port, "0.0.0.0", function() {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
 
-// Part Above is the webserver section DO NOT TOUCH - 
-
-//CODE
-
 var main = function(){
-    $('.icon-menu').click(function(){
-        $('.menu').animate({
-            left:'0px'
+    $(".icon-menu").click(function(){
+        $(".menu").animate({
+            left:"0px"
         },200);
                     
         });
-    $('.icon-close').click(function(){
-        $('.menu').animate({
+    $(".icon-close").click(function(){
+        $(".menu").animate({
             left:"-285px"
         },200);
-   
-    });
-    
-    
+    });    
 };
+
